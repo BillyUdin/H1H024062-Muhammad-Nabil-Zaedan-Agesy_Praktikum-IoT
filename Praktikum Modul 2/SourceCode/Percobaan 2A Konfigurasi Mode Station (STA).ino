9f7d44f4e66409f3093607b4fcf7f11abb99ad33
BillyUdin <zaedanagesy@gmail.com>
2026-09-10 20:43:17 +0700

    Update Percobaan 2A Konfigurasi Mode Station (STA).ino

--- Praktikum Modul 2/SourceCode/Percobaan 2A Konfigurasi Mode Station (STA).ino
+++ Praktikum Modul 2/SourceCode/Percobaan 2A Konfigurasi Mode Station (STA).ino
@@ -1,25 +1,46 @@
-#include <ESP8266WiFi.h>   // ganti dari WiFi.h karena board kamu ESP8266
+#include <WiFi.h>
 
-const char* ap_ssid = "UdinPetot";
-const char* ap_password = "Admin1234"; // minimal 8 karakter
+const char* ssid = "UdinPetot";
+const char* password = "Admin1234";
+
+const int ledPin = 2;    // LED indikator status koneksi
 
 void setup() {
   Serial.begin(115200);
-  // Set mode WiFi menjadi Access Point
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP(ap_ssid, ap_password);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
 
-  IPAddress apIP = WiFi.softAPIP();
-  Serial.println("Access Point aktif!");
-  Serial.print("SSID : ");
-  Serial.println(ap_ssid);
-  Serial.print("IP Address : ");
-  Serial.println(apIP);
+  // Set mode WiFi menjadi Station
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Menghubungkan ke WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Jika berhasil terhubung
+  Serial.println();
+  Serial.println("WiFi berhasil terhubung!");
+  Serial.print("IP Address   : ");
+  Serial.println(WiFi.localIP());
+  Serial.print("MAC Address  : ");
+  Serial.println(WiFi.macAddress());
+  Serial.print("RSSI (dBm)   : ");
+  Serial.println(WiFi.RSSI());
+
+  digitalWrite(ledPin, HIGH);    // nyalakan LED sebagai indikator
 }
 
 void loop() {
-  int jumlahClient = WiFi.softAPgetStationNum();
-  Serial.print("Jumlah perangkat terhubung: ");
-  Serial.println(jumlahClient);
+  // Cek status koneksi setiap 5 detik
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Status: Terhubung");
+  } else {
+    Serial.println("Status: Terputus");
+    digitalWrite(ledPin, LOW);
+  }
+
   delay(5000);
 }
